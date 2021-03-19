0ac8deab74e68515c674fa9d564ab2ccdde6d390
unavailable-sudo <76682714+unavailable-sudo@users.noreply.github.com>
2021-03-19 18:16:48 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/Desktop.js
+++ App/js/Desktop.js
@@ -4,9 +4,19 @@ xui.Class('App.Desktop', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"ctl_pane10")
+                .setDock("fill")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button13")
+                .setLeft("0em")
+                .setTop("43.42857142857143em")
+                .setCaption("Image Button")
+                .setImageClass("xui-icon-xui")
             );
             
             return children;
