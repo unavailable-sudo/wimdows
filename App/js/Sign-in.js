51ef6d59b382cce3c988004548eba412ff5caf12
unavailable-sudo <76682714+unavailable-sudo@users.noreply.github.com>
2021-03-19 18:13:30 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/Sign-in.js
+++ App/js/Sign-in.js
@@ -4,9 +4,22 @@ xui.Class('App.ColumnsNone15', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"ctl_pane10")
+                .setDock("fill")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input18")
+                .setLeft("22.095238095238095em")
+                .setTop("20.571428571428573em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input Password")
+                .setType("password")
+                .setValue("Password")
             );
             
             return children;
